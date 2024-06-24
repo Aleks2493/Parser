6914342f11106c92e143e609600ddb7da18783e1
Aleks2493 <quickly23@yandex.ru>
2024-06-25 00:06:35 +0700

    Отправляет сообщение в бота

--- app/parser.py
+++ app/parser.py
@@ -3,40 +3,55 @@ import asyncio
 import os
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
+from app.database import get_all_users, get_channels, get_keywords
 from aiogram import Bot
-from app.database import get_channels, get_keywords
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
-async def start_telegram_client(bot, user_id):
-    channels = get_channels(user_id)  # Передаем user_id в функцию для получения каналов
-    keywords = get_keywords()  # Функция извлечения ключевых слов из базы данных
+# Здесь необходимо указать ваш токен API Telegram
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
-    async with TelegramClient('session_name', os.getenv('API_ID'), os.getenv('API_HASH'),
-                              device_model="Linux 5.15.0", system_version="Ubuntu 20.04.6 LTS") as client:
-        for channel_link in channels:
-            @client.on(events.NewMessage(chats=channel_link))
-            async def handle_new_message(event):
-                logging.info(f"Новое сообщение в канале {channel_link}: {event.message.message}")
-                print(f"Новое сообщение в канале {channel_link}: {event.message.message}")
+async def start_telegram_client(loop):
+    users = get_all_users()  # Получаем список пользователей из базы данных
 
-                # Проверяем сообщение на наличие ключевых слов
-                for keyword in keywords:
-                    if keyword.lower() in event.message.message.lower():
-                        logging.info(f"Обнаружено ключевое слово '{keyword}' в сообщении: {event.message.message}")
-                        await bot.send_message(chat_id=os.getenv('TELEGRAM_CHAT_ID'),
-                                               text=f"Обнаружено ключевое слово '{keyword}' в сообщении: {event.message.message}")
-                        break  # Прекращаем проверку на другие ключевые слова
+    async with TelegramClient('session_name', os.getenv('API_ID'), os.getenv('API_HASH'), device_model="Linux 5.15.0", system_version="Ubuntu 20.04.6 LTS", loop=loop) as client:
+        for user_id in users:
+            channels = get_channels(user_id)  # Получаем каналы для текущего пользователя
+            for channel_link in channels:
+                @client.on(events.NewMessage(chats=channel_link))
+                async def handle_new_message(event):
+                    logging.info(f"Новое сообщение в канале {channel_link}: {event.message.message}")
 
-            logging.info(f"Мониторинг канала {channel_link}")
+                    keywords = get_keywords(user_id)  # Получаем список ключевых слов из базы данных
+                    for keyword in keywords:
+                        if keyword.lower() in event.message.message.lower():
+                            logging.info(f"Найдено ключевое слово '{keyword}' в сообщении")
+
+
+                            # Отправляем сообщение в бота
+                            await send_to_bot(user_id, channel_link, event.message.message)
+
+                logging.info(f"Мониторинг канала {channel_link}")
 
         logging.info("Сессия Telethon подключена")
         
         # Запускаем клиента Telegram и ждем событий
         await client.run_until_disconnected()
 
-def run_telegram_monitoring(bot, user_id):
+async def send_to_bot(user_id, channel_link, message):
+    # Функция для отправки сообщения в бота
+
+    bot = Bot(token=TELEGRAM_TOKEN)
+    try:
+        await bot.send_message(user_id, f"Новое сообщение в канале: {channel_link}, от пользователя:{user_id}:\n{message}")
+        logging.info(f"Сообщение успешно отправлено в бота пользователю {user_id}")
+    except Exception as e:
+        logging.error(f"Ошибка при отправке сообщения в бота: {e}")
+
+def run_telegram_monitoring():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(start_telegram_client(bot, user_id))
+    last_message = loop.run_until_complete(start_telegram_client(loop))
+    loop.close()
+    return last_message
