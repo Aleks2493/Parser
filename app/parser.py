31888393f469f85ee5fd4717781d1dac113e7aea
Aleks2493 <quickly23@yandex.ru>
2024-06-24 08:54:01 +0700

    24,06,2024

--- app/parser.py
+++ app/parser.py
@@ -1,49 +1,128 @@
 import logging
 import asyncio
 import os
+import json
+from telethon import connection
 from dotenv import load_dotenv
 from threading import Thread
 from telethon import TelegramClient, events
-from app.database import get_channels, get_keywords, get_all_users
 from aiogram import Bot
+# для корректного переноса времени сообщений в json
+from datetime import date, datetime
+
+# классы для работы с каналами
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
+
+# класс для работы с сообщениями
+from telethon.tl.functions.messages import GetHistoryRequest
 
 
-async def notify_user(bot, user_id, message):
-    await bot.send_message(user_id, message, parse_mode='Markdown')
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
-async def monitor_groups(client, bot):
-    @client.on(events.NewMessage)
-    async def handler(event):
-        try:
-            chat_id = event.chat_id
-            message_text = event.message.message
-            user_id = event.sender_id
+async def dump_all_messages():
+	client = TelegramClient('session_name', os.getenv('API_ID'), os.getenv('API_HASH'), loop=loop)
+	await client.start()
+	channel = 2208551919
+	"""Записывает json-файл с информацией о всех сообщениях канала/чата"""
+	offset_msg = 0    # номер записи, с которой начинается считывание
+	limit_msg = 100   # максимальное число записей, передаваемых за один раз
 
-            users = get_all_users()
-            for user in users:
-                channels = get_channels(user)
-                keywords = get_keywords(user)
+	all_messages = []   # список всех сообщений
+	total_messages = 0
+	total_count_limit = 0  # поменяйте это значение, если вам нужны не все сообщения
 
-                if chat_id in channels:
-                    if any(keyword.lower() in message_text.lower() for keyword in keywords):
-                        chat_info = f"Чат: {chat_id}"
-                        user_info = f"Пользователь: {user_id}"
-                        message_link = f"https://t.me/{event.chat.username}/{event.message.id}"
-                        notification_message = f"Найдено ключевое слово!\n{chat_info}\n{user_info}\n[Ссылка на сообщение]({message_link})"
-                        await notify_user(bot, user, notification_message)
+	class DateTimeEncoder(json.JSONEncoder):
+		'''Класс для сериализации записи дат в JSON'''
+		def default(self, o):
+			if isinstance(o, datetime):
+				return o.isoformat()
+			if isinstance(o, bytes):
+				return list(o)
+			return json.JSONEncoder.default(self, o)
 
-        except Exception as e:
-            logging.error(f"Ошибка при мониторинге канала: {e}")
+	while True:
+		history = await client(GetHistoryRequest(
+			peer=channel,
+			offset_id=offset_msg,
+			offset_date=None, add_offset=0,
+			limit=limit_msg, max_id=0, min_id=0,
+			hash=0))
+		if not history.messages:
+			break
+		messages = history.messages
+		for message in messages:
+			all_messages.append(message.to_dict())
+		offset_msg = messages[len(messages) - 1].id
+		total_messages = len(all_messages)
+		if total_count_limit != 0 and total_messages >= total_count_limit:
+			break
 
-async def start_telethon_client(loop, bot):
-    load_dotenv()
-    client = TelegramClient('session_name', os.getenv('API_ID'), os.getenv('API_HASH'), loop=loop)
-    await client.start()
-    await monitor_groups(client, bot)
-    print("Начинаем мониторинг...")
-    await client.run_until_disconnected()
+	with open('channel_messages.json', 'w', encoding='utf8') as outfile:
+		 json.dump(all_messages, outfile, ensure_ascii=False, cls=DateTimeEncoder)
 
-def run_telethon_client(bot):
+
+
+def run_telegram_monitoring():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(start_telethon_client(loop, bot))
+    loop.run_until_complete(dump_all_messages(loop))
+
+# async def monitor_channel(client, bot, channel_link):
+#     try:
+#         entity = await client.get_entity(channel_link)
+#         channel_id = entity.id
+
+#         print(f'Канал: {channel_id}')
+#         # Преобразуем channel_id в формат -100xxxxxxxxxx
+#         if channel_id > 0:
+#             channel_id = int(f'-100{channel_id}')
+#         print(f'Канал после: {channel_id}')
+
+#         @client.on(events.NewMessage)
+#         async def handler(event):
+#             print(event.chat.name, ':', event.text)
+
+#             messages = client.get_messages(channel_id, 1)
+#             print(messages)
+#             try:
+#                 message = event.message
+#                 message_text = message.message if message else None
+
+#                 logging.info(f"Получено сообщение: {message_text} в канале {channel_link}")
+
+#                 if message_text:
+#                     logging.info(f"Сообщение: {message_text}")
+#                 else:
+#                     logging.info("Сообщение не содержит текста.")
+#             except Exception as e:
+#                 logging.error(f"Ошибка при обработке сообщения: {e}")
+
+#     except Exception as e:
+#         logging.error(f"Ошибка при мониторинге канала: {e}")
+
+# async def start_telegram_client(loop):
+#     client = TelegramClient('session_name', os.getenv('API_ID'), os.getenv('API_HASH'), loop=loop)
+#     await client.start()
+#     print("Сессия подключена...")
+#     id_channel = 2208551919
+    
+#     last_message = await client.get_entity(id_channel)
+#     last_message2 = await client.get_input_entity(id_channel)
+#     entity = await client.get_entity('someone')
+#     last_message3 = await client.get_messages(id_channel, 0)
+#     # async for message in client.iter_messages(id_channel):
+#     #     print(message.id, message.text)
+
+
+#     if last_message:
+#         print(f"Последнее сообщение1: {last_message}")
+#         print(f"Последнее сообщение2: {last_message2}")
+#         print(f"Последнее сообщение3: {entity}")
+#         print(f"Последнее сообщение4: {last_message3}")
+#     else:
+#         print("Сообщений нет")
+
+#     print("Начинаем мониторинг...")
+    # await client.run_until_disconnected()
--- main.py
+++ main.py
@@ -7,25 +7,23 @@ from aiogram import Bot, Dispatcher
 from app.handlers import router
 from aiogram.client.bot import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
-from app.parser import run_telethon_client
+from app.parser import run_telegram_monitoring
 
 # Включаем логирование
 logging.basicConfig(level=logging.INFO)
 
-async def main():       
-    load_dotenv()   
-    bot = Bot(token=os.getenv('TELEGRAM_TOKEN'), default=DefaultBotProperties(parse_mode='HTML'))         #Подключение к боту по токену, при Proxy добавить (, session=session)
-    dp = Dispatcher(storage=MemoryStorage())  
+async def main():
+    load_dotenv()
+    bot = Bot(token=os.getenv('TELEGRAM_TOKEN'), default=DefaultBotProperties(parse_mode='HTML'))
+    dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
-    telethon_thread = Thread(target=run_telethon_client, args=(bot,), daemon=True)
+    telethon_thread = Thread(target=run_telegram_monitoring, daemon=True)# args=(bot,),
     telethon_thread.start()
-    await dp.start_polling(bot)          #Отправляют запрос на сервер ТГ, если ответ есть, то он вернет его.
+    await dp.start_polling(bot)
 
-
-
-if __name__ == '__main__':               
-    try:                                 
-        print('Бот работает')  
-        asyncio.run(main())       
+if __name__ == '__main__':
+    try:
+        print('Бот работает')
+        asyncio.run(main())
     except KeyboardInterrupt:
         print('Бот остановлен')
